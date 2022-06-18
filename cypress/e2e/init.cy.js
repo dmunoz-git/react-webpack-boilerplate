984d9df2c2c63cd5bf0553b31e738a58552f63bc
dmunoz-git <dmunozGit@gmail.com>
2022-06-18 16:12:09 +0200

    chore: :heavy_minus_sign: removes cypress for a clean install

--- cypress/e2e/init.cy.js
+++
@@ -1,6 +0,0 @@
-describe("First cypress test", () => {
-    it("should visit the app and check if contains an logo", () => {
-        cy.visit("http://localhost:3000");
-        cy.contains("logo");
-    });
-})
