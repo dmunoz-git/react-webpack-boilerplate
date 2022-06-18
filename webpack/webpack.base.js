fabf3a0a9c45fbf648beeefbb2720fff9e35da08
dmunoz-git <dmunozGit@gmail.com>
2022-06-18 15:59:34 +0200

    fix: :bug: move builds back to root path of the project

--- webpack/webpack.base.js
+++ webpack/webpack.base.js
@@ -6,7 +6,7 @@ module.exports = {
   stats: "minimal",
 
   output: {
-    path: path.resolve(__dirname, "build"),
+    path: path.resolve(__dirname, "../build"),
     clean: true,
   },
 
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,5 +1,6 @@
 module.exports = {
   mode: "development",
+  
   output: {
     filename: "[name].js",
   },
