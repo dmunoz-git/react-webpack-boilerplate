990ab3a8fe703ee0399a805bdfc50edfa3b0141e
dmunoz-git <dmunozGit@gmail.com>
2022-07-02 20:03:22 +0200

    fix: :art: fix devDependencies and alias path eslint rules

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
+/* eslint-disable global-require */
 const { merge } = require('webpack-merge');
 const baseConfig = require('./webpack/webpack.base');
 
 module.exports = (env, argv) => {
   const enviroment = argv.mode === 'production' ? 'prod' : 'dev';
 
+  // eslint-disable-next-line import/no-dynamic-require
   const config = require(`./webpack/webpack.${enviroment}.js`);
 
   return merge(baseConfig, config);
